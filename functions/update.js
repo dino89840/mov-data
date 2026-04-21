f058579a552d5e5025d59dd24fa533cd63ab4306
dino89840 <dino89490@gmail.com>
2026-04-22 02:05:18 +0630

    Update update.js

--- functions/update.js
+++ functions/update.js
@@ -2,11 +2,17 @@ export async function onRequestPost(context) {
     const { env } = context;
     try {
         const body = await context.request.json();
+        const SECURE_PASSWORD = env.ADMIN_PASSWORD; 
+
+        // Password စစ်ဆေးခြင်း
+        if (body.password !== SECURE_PASSWORD) {
+            return new Response("Unauthorized: Wrong Password", { status: 401 });
+        }
         
-        // body.genre ကို Key လုပ်ပြီး body.data ကို သိမ်းမယ်
+        // Data သိမ်းဆည်းခြင်း
         await env.MOVIE_DB.put(body.genre, body.data);
+        return new Response("Success: Data Updated!", { status: 200 });
         
-        return new Response("Update Successful", { status: 200 });
     } catch (e) {
         return new Response("Error: " + e.message, { status: 500 });
     }
